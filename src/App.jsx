58b8687ab490409435bbae12d4aeaf749dc38a12
Michael Peter <michaelhpet@gmail.com>
2023-09-23 12:25:49 +0100

    feat: complete checkpoint solution

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,39 @@
+import { products } from "./products";
+import Card from "react-bootstrap/Card";
+import Button from "react-bootstrap/Button";
+
 export default function App() {
-  return <h1>Hello World</h1>;
+  const myName = "Michael Peter";
+
+  return (
+    <main>
+      <header>
+        <h1>JSX and React Components Checkpoint</h1>
+      </header>
+
+      <div className="products">
+        {products.map((product, index) => (
+          <Card key={index} style={{ width: "18rem" }}>
+            <Card.Img
+              variant="top"
+              src={product.imageUrl}
+              width="100%"
+              height={200}
+            />
+            <Card.Body>
+              <Card.Title>{product.name}</Card.Title>
+              <Card.Subtitle>{product.price}</Card.Subtitle>
+              <Card.Text>{product.description}</Card.Text>
+              <Button variant="primary">Add to cart</Button>
+            </Card.Body>
+          </Card>
+        ))}
+      </div>
+
+      <article>
+        <h3>{myName}</h3>
+        <h4>September 23, 2023</h4>
+      </article>
+    </main>
+  );
 }
---
+++ src/products.js
@@ -0,0 +1,25 @@
+export const products = [
+  {
+    name: "MacBook Air 2022",
+    price: "$1,500",
+    description:
+      "MacBook Air laptop, now in 13-inch and 15-inch models. With the blazing-fast M2 chip, all-day battery life, and a stunning Liquid Retina display.",
+    imageUrl:
+      "https://images.unsplash.com/photo-1660945742817-217842d82b55?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1887&q=80",
+  },
+  {
+    name: "Darter Pro",
+    price: "$1,049",
+    description:
+      "The Darter Pro is small for its 15” size. A short depth and thin bezel make this a highly portable laptop. With the lid, top case, and bottom case.",
+    imageUrl:
+      "https://images.prismic.io/system76/43344110-ce31-45db-a691-3a61632d3e78_darp6_angle_extra1+4.png?auto=compress,format",
+  },
+  {
+    name: "Surface Laptop Go",
+    price: "$675",
+    description:
+      "Microsoft Surface Laptop Go. Light, portable Surface touchscreen Laptop with Windows 11 and 12.4” PixelSense Display. Starting at just 2.48 pounds.",
+    imageUrl: "https://m.media-amazon.com/images/I/51ytgQ3SonL._AC_SL1080_.jpg",
+  },
+];
